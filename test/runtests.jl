2e9f23034eb5b73920138f86aeb8af8bba780165
Chris Foster <chris.foster@roames.com.au>
2017-04-24 18:07:57 +1000

    Rearrange tests into test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,87 +33,101 @@ function centroid(io, header)
     x_avg, y_avg, z_avg
 end
 
-# reading point by point
-open(testfile) do io
-    header = read(io, LasHeader12)
+@testset "LasIO" begin
 
-    seek(io, header.data_offset)
-    x_avg, y_avg, z_avg = centroid(io, header)
 
-    @test x_avg ≈ 1442694.2739025319
-    @test y_avg ≈ 377449.24373880465
-    @test z_avg ≈ 861.60254888088491
+@testset "Loading and saving" begin
+    # reading point by point
+    open(testfile) do io
+        header = read(io, LasHeader12)
 
-    seek(io, header.data_offset)
-    p = read(io, LasPoint0)
+        seek(io, header.data_offset)
+        x_avg, y_avg, z_avg = centroid(io, header)
 
-    @test xcoord(p, header) ≈ 1.44013394e6
-    @test xcoord(1.44013394e6, header) ≈ p.x
-    @test ycoord(p, header) ≈ 375000.23
-    @test ycoord(375000.23, header) ≈ p.y
-    @test zcoord(p, header) ≈ 846.66
-    @test zcoord(846.66, header) ≈ p.z
-    @test intensity(p) === 0x00fa
-    @test scan_angle(p) === 0x00
-    @test user_data(p) === 0x00
-    @test pt_src_id(p) === 0x001d
-    @test return_number(p) === 0x00
-    @test number_of_returns(p) === 0x00
-    @test scan_direction(p) === false
-    @test edge_of_flight_line(p) === false
-    @test classification(p) === 0x02
-    @test synthetic(p) === false
-    @test key_point(p) === false
-    @test withheld(p) === false
+        @test x_avg ≈ 1442694.2739025319
+        @test y_avg ≈ 377449.24373880465
+        @test z_avg ≈ 861.60254888088491
 
-    # raw bytes composed of bit fields
-    @test flag_byte(p) === 0x00
-    @test raw_classification(p) === 0x02
+        seek(io, header.data_offset)
+        p = read(io, LasPoint0)
 
-    # recompose bytes with bit fields
-    @test flag_byte(return_number(p),number_of_returns(p),scan_direction(p),edge_of_flight_line(p)) === p.flag_byte
-    @test raw_classification(classification(p),synthetic(p),key_point(p),withheld(p)) === p.raw_classification
+        @test xcoord(p, header) ≈ 1.44013394e6
+        @test xcoord(1.44013394e6, header) ≈ p.x
+        @test ycoord(p, header) ≈ 375000.23
+        @test ycoord(375000.23, header) ≈ p.y
+        @test zcoord(p, header) ≈ 846.66
+        @test zcoord(846.66, header) ≈ p.z
+        @test intensity(p) === 0x00fa
+        @test scan_angle(p) === 0x00
+        @test user_data(p) === 0x00
+        @test pt_src_id(p) === 0x001d
+        @test return_number(p) === 0x00
+        @test number_of_returns(p) === 0x00
+        @test scan_direction(p) === false
+        @test edge_of_flight_line(p) === false
+        @test classification(p) === 0x02
+        @test synthetic(p) === false
+        @test key_point(p) === false
+        @test withheld(p) === false
 
-    # TODO GPS time, colors (not in this test file, is point data format 0)
+        # raw bytes composed of bit fields
+        @test flag_byte(p) === 0x00
+        @test raw_classification(p) === 0x02
+
+        # recompose bytes with bit fields
+        @test flag_byte(return_number(p),number_of_returns(p),scan_direction(p),edge_of_flight_line(p)) === p.flag_byte
+        @test raw_classification(classification(p),synthetic(p),key_point(p),withheld(p)) === p.raw_classification
+
+        # TODO GPS time, colors (not in this test file, is point data format 0)
+    end
+
+    # reading complete file into memory
+    # test if output file matches input file
+    header, pointdata = load(testfile)
+    n = length(pointdata)
+    save(writefile, header, pointdata)
+    @test hash(read(testfile)) == hash(read(writefile))
+    rm(writefile)
+
+    # memory mapping the point data
+    open(testfile) do io
+        seek(io, header.data_offset)
+        ptdata = Mmap.mmap(io, Vector{LasPoint0}, n)
+        @test ptdata == pointdata
+    end
+
 end
 
-# reading complete file into memory
-# test if output file matches input file
-header, pointdata = load(testfile)
-n = length(pointdata)
-save(writefile, header, pointdata)
-@test hash(read(testfile)) == hash(read(writefile))
-rm(writefile)
 
-# memory mapping the point data
-open(testfile) do io
-    seek(io, header.data_offset)
-    ptdata = Mmap.mmap(io, Vector{LasPoint0}, n)
-    @test ptdata == pointdata
+@testset "VLRs" begin
+
+    # testing a las file version 1.0 point format 1 file with VLRs
+    srsfile = joinpath(workdir, "srs.las")
+    srsfile_out = joinpath(workdir, "srs-out.las")
+    srsheader, srspoints = load(srsfile)
+    @test srsheader.version_major == 1
+    @test srsheader.version_minor == 0
+    @test srsheader.data_format_id == 1
+    @test srsheader.n_vlr == 3
+    @test isa(srsheader.variable_length_records, Vector{LasVariableLengthRecord})
+    for vlr in srsheader.variable_length_records
+        @test vlr.reserved === 0xaabb
+        @test vlr.user_id == "LASF_Projection"
+        @test vlr.description == ""
+    end
+
+    @test srsheader.variable_length_records[1].record_id == 34735  # GeoKeyDirectoryTag
+    @test srsheader.variable_length_records[2].record_id == 34736  # GeoDoubleParamsTag
+    @test srsheader.variable_length_records[3].record_id == 34737  # GeoAsciiParamsTag
+    @test srsheader.variable_length_records[1].data[1:4] == [0x01,0x00,0x01,0x00]
+    @test all(x -> x === 0x00, srsheader.variable_length_records[2].data)
+    @test all(x -> x === 0x00, srsheader.variable_length_records[3].data)
+
+    save(srsfile_out, srsheader, srspoints)
+    @test hash(read(srsfile)) == hash(read(srsfile_out))
+    rm(srsfile_out)
+
 end
 
-# testing a las file version 1.0 point format 1 file with VLRs
-srsfile = joinpath(workdir, "srs.las")
-srsfile_out = joinpath(workdir, "srs-out.las")
-srsheader, srspoints = load(srsfile)
-@test srsheader.version_major == 1
-@test srsheader.version_minor == 0
-@test srsheader.data_format_id == 1
-@test srsheader.n_vlr == 3
-@test isa(srsheader.variable_length_records, Vector{LasVariableLengthRecord})
-for vlr in srsheader.variable_length_records
-    @test vlr.reserved === 0xaabb
-    @test vlr.user_id == "LASF_Projection"
-    @test vlr.description == ""
+
 end
-
-@test srsheader.variable_length_records[1].record_id == 34735  # GeoKeyDirectoryTag
-@test srsheader.variable_length_records[2].record_id == 34736  # GeoDoubleParamsTag
-@test srsheader.variable_length_records[3].record_id == 34737  # GeoAsciiParamsTag
-@test srsheader.variable_length_records[1].data[1:4] == [0x01,0x00,0x01,0x00]
-@test all(x -> x === 0x00, srsheader.variable_length_records[2].data)
-@test all(x -> x === 0x00, srsheader.variable_length_records[3].data)
-
-save(srsfile_out, srsheader, srspoints)
-@test hash(read(srsfile)) == hash(read(srsfile_out))
-rm(srsfile_out)
