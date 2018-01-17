7a04e0469d5bc98ab9346d8df4b51a585e5402f1
Maarten Pronk <git@evetion.nl>
2018-01-17 14:19:28 +0100

    Also replaced stream by mmap in tests.

--- test/stream.jl
+++ test/stream.jl
@@ -9,7 +9,7 @@ testfile = joinpath(workdir, filename)
 writefile = joinpath(workdir, "libLAS_1.2-out.las")
 
 # test if output file matches input file
-header, pointdata = load(testfile, stream=true)
+header, pointdata = load(testfile, mmap=true)
 n = length(pointdata)
 save(writefile, header, pointdata)
 @test hash(read(testfile)) == hash(read(writefile))
@@ -18,7 +18,7 @@ rm(writefile)
 # testing a las file version 1.0 point format 1 file with VLRs
 srsfile = joinpath(workdir, "srs.las")
 srsfile_out = joinpath(workdir, "srs-out.las")
-srsheader, srspoints = load(srsfile, stream=true)
+srsheader, srspoints = load(srsfile, mmap=true)
 for record in srsheader.variable_length_records
     @test record.reserved === 0xaabb
     @test record.user_id == "LASF_Projection"
@@ -65,5 +65,5 @@ rm(srsfile_out)
 # Test editing stream file
 srsfile = joinpath(workdir, "srs.las")
 srsfile_out = joinpath(workdir, "srs-out.las")
-srsheader, srspoints = load(srsfile, stream=true)
+srsheader, srspoints = load(srsfile, mmap=true)
 @test_throws ErrorException srspoints[5] = LasPoint1(1,1,1,1,1,1,1,1,1,1.0)
